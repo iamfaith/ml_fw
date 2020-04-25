7ab1e29c1fd51adf2eb9734feea7f0330c4fb527
xianzixiang <xianzixiang@wps.cn>
2020-04-24 21:13:16 -0400

    new

--- model/model_factory.py
+++ model/model_factory.py
@@ -1,4 +1,7 @@
-
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models
+import pretrainedmodels
 
 
 def keras_InceptionV3(num_classes=11):
@@ -20,3 +23,78 @@ def keras_InceptionV3(num_classes=11):
     return InceptionV3_model
     # InceptionV3_model.summary()
     # print(len(InceptionV3_model.layers))
+    
+    
+def get_resnet34(num_classes=28, **_):
+    model_name = 'resnet34'
+    model = pretrainedmodels.__dict__[model_name](num_classes=1000, pretrained='imagenet')
+    conv1 = model.conv1
+    model.conv1 = nn.Conv2d(in_channels=4,
+                            out_channels=conv1.out_channels,
+                            kernel_size=conv1.kernel_size,
+                            stride=conv1.stride,
+                            padding=conv1.padding,
+                            bias=conv1.bias)
+
+    # copy pretrained weights
+    model.conv1.weight.data[:,:3,:,:] = conv1.weight.data
+    model.conv1.weight.data[:,3:,:,:] = conv1.weight.data[:,:1,:,:]
+
+    model.avgpool = nn.AdaptiveAvgPool2d(1)
+    in_features = model.last_linear.in_features
+    model.last_linear = nn.Linear(in_features, num_classes)
+    return model
+
+
+def get_resnet18(num_classes=28, **_):
+    model_name = 'resnet18'
+    model = pretrainedmodels.__dict__[model_name](num_classes=1000, pretrained='imagenet')
+    conv1 = model.conv1
+    model.conv1 = nn.Conv2d(in_channels=4,
+                            out_channels=conv1.out_channels,
+                            kernel_size=conv1.kernel_size,
+                            stride=conv1.stride,
+                            padding=conv1.padding,
+                            bias=conv1.bias)
+
+    # copy pretrained weights
+    model.conv1.weight.data[:,:3,:,:] = conv1.weight.data
+    model.conv1.weight.data[:,3:,:,:] = conv1.weight.data[:,:1,:,:]
+
+    model.avgpool = nn.AdaptiveAvgPool2d(1)
+    in_features = model.last_linear.in_features
+    model.last_linear = nn.Linear(in_features, num_classes)
+    return model
+
+
+def get_senet(model_name='se_resnext50', num_classes=28, **_):
+    model = pretrainedmodels.__dict__[model_name](num_classes=1000, pretrained='imagenet')
+    conv1 = model.layer0.conv1
+    model.layer0.conv1 = nn.Conv2d(in_channels=4,
+                                   out_channels=conv1.out_channels,
+                                   kernel_size=conv1.kernel_size,
+                                   stride=conv1.stride,
+                                   padding=conv1.padding,
+                                   bias=conv1.bias)
+
+    # copy pretrained weights
+    model.layer0.conv1.weight.data[:,:3,:,:] = conv1.weight.data
+    model.layer0.conv1.weight.data[:,3:,:,:] = conv1.weight.data[:,:1,:,:]
+
+    model.avgpool = nn.AdaptiveAvgPool2d(1)
+    in_features = model.last_linear.in_features
+    model.last_linear = nn.Linear(in_features, num_classes)
+    return model
+
+
+def get_se_resnext50(num_classes=28, **kwargs):
+    return get_senet('se_resnext50_32x4d', num_classes=num_classes, **kwargs)
+
+
+def get_model(config):
+    print('model name:', config.model.name)
+    f = globals().get('get_' + config.model.name)
+    if config.model.params is None:
+        return f()
+    else:
+        return f(**config.model.params)
