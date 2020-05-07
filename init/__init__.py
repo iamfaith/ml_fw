3a07ff5e29676577a8c815af5052b38b372f5b33
xianzixiang <xianzixiang@wps.cn>
2020-05-06 21:25:47 -0400

    new

--- init/__init__.py
+++ init/__init__.py
@@ -8,3 +8,15 @@ abs_path = os.path.abspath('./')
 sys.path.append(abs_path)
 from init.load import init_cfg
 cfg = init_cfg(os.path.join(abs_path + '/config', 'example.policy.py'))
+
+
+def rename_project(name):
+    cfg['project'] = name
+    
+
+from datetime import datetime
+
+rename_project(str(datetime.now()))
+
+# print(cfg)
+# os.mkdir(str(datetime.now()))
